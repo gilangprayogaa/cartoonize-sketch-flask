8c5f248ead3ace708a45e60a71876e26c6d7c1e3
samirkhanal35 <samirkhanal35@gmail.com>
2020-09-13 16:24:00 +0545

    heroku requirements fulfilled

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template   
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
 
+app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template("home.html")
 
 
-
 if __name__ == '__main__':
+   # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
    app.run(debug = True)
