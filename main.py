dc2b34e5b0997d71b049fbcc65e0c1cf0a319fbd
samirkhanal35 <samirkhanal35@gmail.com>
2020-09-13 16:14:24 +0545

    heroku requirements fulfilled

--- main.py
+++ main.py
@@ -1,58 +1,14 @@
-import os
-from flask import Flask, flash, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
-import cv2
-import numpy as np
-from cartoonize import cartoon
-
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-app = Flask(__name__, template_folder='templates')
-
-# app.secret_key = "secret key"
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+from flask import Flask, render_template   
+app = Flask(__name__)
 
 
 @app.route('/')
-def upload_form():
-	return "samir"
+def home():
+   return render_template("home.html")
 
-# @app.route('/img', methods=['POST'])
-# def upload_image():
-# 	if 'file' not in request.files:
-# 		flash('No file part')
-# 		return redirect(request.url)
-# 	file = request.files['file']
-# 	if file.filename == '':
-# 		flash('No image selected for uploading')
-# 		return redirect(request.url)
-# 	if file and allowed_file(file.filename):
-# 		filename = secure_filename(file.filename)
-# 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-# 		#print('upload_image filename: ' + filename)
-# 		flash('Image successfully uploaded and displayed')
-# 		return render_template('home.html', filename=filename)
-# 	else:
-# 		flash('Allowed image types are -> png, jpg, jpeg')
-# 		return redirect(request.url)
+@app.route('/about')
+def about():
+   return render_template("about.html")
 
-
-# @app.route('/display/<filename>')
-# def display_image(filename):
-	
-#     img = cv2.imread('static/uploads/'+filename)
-
-#     img = cartoon(img)
-
-#     ret, jpeg = cv2.imencode('.jpg', img)
-#     os.remove('static/uploads/'+filename)
-#     return jpeg.tobytes()
-
-
-
+if __name__ == '__main__':
+   app.run(debug = True)
