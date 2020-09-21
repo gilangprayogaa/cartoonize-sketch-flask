e86d31549174ad59e0ee733f8ebc90d74908187e
samirkhanal35 <samirkhanal35@gmail.com>
2020-09-21 16:58:43 +0545

    test

--- main.py
+++ main.py
@@ -17,30 +17,52 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
-def home():
-   return render_template("home.html")
+# @app.route('/')
+# def home():
+#    return render_template("home.html")
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No image selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            process_file(os.path.join(UPLOAD_FOLDER, filename), filename)
+            data={
+                "uploaded_img":'static/uploads/'+filename
+            }
+            return render_template("home.html",data=data, filename=filename)  
+		else:
+			flash('Allowed image types are -> png, jpg, jpeg')
+			return redirect(request.url)
+    return render_template('home.html')
 
-@app.route('/img', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-        # filename = file.filename
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed')
-		return render_template('home.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg')
-		return redirect(request.url)
+# @app.route('/img', methods=['POST'])
+# def upload_image():
+# 	if 'file' not in request.files:
+# 		flash('No file part')
+# 		return redirect(request.url)
+# 	file = request.files['file']
+# 	if file.filename == '':
+# 		flash('No image selected for uploading')
+# 		return redirect(request.url)
+# 	if file and allowed_file(file.filename):
+# 		filename = secure_filename(file.filename)
+#         # filename = file.filename
+# 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+# 		#print('upload_image filename: ' + filename)
+# 		flash('Image successfully uploaded and displayed')
+# 		return render_template('home.html', filename=filename)
+# 	else:
+# 		flash('Allowed image types are -> png, jpg, jpeg')
+# 		return redirect(request.url)
 
 # @app.route('/display/<filename>')
 # def display_image(filename):	
